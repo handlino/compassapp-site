3cce6a6a5c7db3a41668c8dbdc4774c2f691dd83
tka lu <tka@mail2000.com.tw>
2010-12-28 19:47:40 +0800

    add capify

---
+++ config/deploy.rb
@@ -0,0 +1,30 @@
+set :application, "compass.app-site"
+set :repository,  "git@git.handlino.com:compass-site.git"
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :branch, "master" # or production
+set :deploy_via, :remote_cache
+
+set :deploy_to, "/home/www/compass.app"
+set :user, "railsapp"
+set :port, "723"
+set :copy_exclude, ".git"
+default_run_options[:pty] = true
+
+set :use_sudo, false
+role :web, "git.handlino.com"                          # Your HTTP server, Apache/etc
+role :app, "git.handlino.com"                          # This may be the same as your `Web` server
+#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
